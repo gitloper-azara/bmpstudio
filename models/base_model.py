d497b3b5a30f8adf72008ae75778b3e1801283a4
Azara <yyushahu02@gmail.com>
2024-03-02 11:32:36 +0000

    include call to save and new methods of storage in basemodel and authors file

--- models/base_model.py
+++ models/base_model.py
@@ -29,6 +29,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self) -> str:
         '''Returns a string representation of class.name, id and dict'''
@@ -39,6 +40,7 @@ class BaseModel():
         datetime
         '''
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self) -> dict:
         '''Returns a dictionary containing all keys/values of __dict__
